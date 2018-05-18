2f89fa664c9926da0d827d00b664726156c361e1
Felipe Barboza <fbs@poli.ufrj.br>
2018-05-18 16:44:02 -0300

    Plotting script updated

--- plot_current_voltage_power.m
+++ plot_current_voltage_power.m
@@ -32,60 +32,71 @@ close all;
 % 60	5.9	237.8
 % 65	6.2	240.5];
 
-load DCeVDC.mat;
+addpath(['.' filesep 'VLC_param' filesep]);
+addpath(['.' filesep 'VLC_param' filesep 'LED Parameters' filesep]);
 
-opticalPower = [0.05
-0.554
-1.47
-3
-5.2
-7.5
-11.05
-15.68
-21.81
-29.05
-36.87
-45.2
-53.78
-62.53
-71.33
-80.3
-89.48
-96.55
-97.08
-97.02
-96.97
-96.84
-96.82
-96.75
-96.74
-96.6
-96.55
-96.63
-96.59
-96.53] * 1e-6;
+
+if isunix
+    addpath('/home/felipe/Dropbox/VLC_exp/data');
+    addpath('/home/felipe.silva/Dropbox/VLC_exp/data');
+else
+    addpath('C:\Users\Felipe Barboza\Dropbox\VLC_exp\data');
+end
+
+load VDCxDCxP.mat;
+P = P*1e-6;
+
+% load DCeVDC.mat;
+
+% opticalPower = [0.05
+% 0.554
+% 1.47
+% 3
+% 5.2
+% 7.5
+% 11.05
+% 15.68
+% 21.81
+% 29.05
+% 36.87
+% 45.2
+% 53.78
+% 62.53
+% 71.33
+% 80.3
+% 89.48
+% 96.55
+% 97.08
+% 97.02
+% 96.97
+% 96.84
+% 96.82
+% 96.75
+% 96.74
+% 96.6
+% 96.55
+% 96.63
+% 96.59
+% 96.53] * 1e-6;
 receiverDiameter = 3.2e-3;
 
 area = pi*(receiverDiameter/2)^2;
 
 plot(VDC, DC)
 
-aux = find(round(VDC,2)==2.35);
 figure
-plot(DC(aux:length(opticalPower)+aux-1),opticalPower/area)
+plot(DC,P/area)
 
 figure
-plot(DC(aux:length(opticalPower)+aux-1).* VDC(aux:length(opticalPower)+aux-1),opticalPower/area)
-
-
-
-    
+plot(DC.* VDC,P/area)
 
 
 load whiteLED_334-15_Param.mat;
 load whiteLED_334-15.mat;
 load VLC_param01.mat;
 
+% maxLuminousIntensityLED = 10000;
+% k = 1;
 eletricalPowerOutput = 0:0.01:0.3;
 opticalPowerOutput = Poptical(ledLuminousEfficacy,eletricalPowerOutput,kNonLinearity,maxLuminousIntensityLED);
 
@@ -94,17 +105,26 @@ figure
 plot(eletricalPowerOutput, opticalPowerOutput)
 
 modFun = @(vmax, vdc) (vmax - vdc)/vdc; 
-vmaxFun = @(MI, vdc) (vdc.*MI);
+vmaxFun = @(MI, vdc) kron(vdc,MI);
 
-voltageVector = [2.8 3];
+voltageVector = [3.4 3.5 3.6];
 
  %quando a tens�o caiu 4 vezes do valor inicial;
 % 
 % vdc = data(:,2);
-MI = [0.1];
+MI = [0.05 0.075 0.1];
 % 
 vmax = vmaxFun(MI,voltageVector)*2000; %Vpp [mV]
 
+rmpath(['.' filesep 'VLC_param' filesep]);
+rmpath(['.' filesep 'VLC_param' filesep 'LED Parameters' filesep]);
 
 
+if isunix
+    rmpath('/home/felipe/Dropbox/VLC_exp/data');
+    rmpath('/home/felipe.silva/Dropbox/VLC_exp/data');
+    addpath(['.' filesep 'Utils' filesep]);
+else
+    rmpath('C:\Users\Felipe Barboza\Dropbox\VLC_exp\data');
+end
 
